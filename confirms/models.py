a87cd91c34312334be43a6b18c1655720ca02fe2
Erick Otenyo <otenyo.erick@gmail.com>
2017-03-16 15:28:10 +0300

    added Deny class

--- confirms/models.py
+++ confirms/models.py
@@ -5,8 +5,6 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-from tukiogram.models import Tukio
-
 
 class Confirm(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL)
@@ -14,6 +12,18 @@ class Confirm(models.Model):
 	object_id = models.PositiveIntegerField()
 	content_object = GenericForeignKey('content_type', 'object_id')
 	confirm = models.BooleanField(default=False)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	
+	def __unicode__(self):
+		return str(self.user.first_name)
+
+
+class Deny(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL)
+	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+	object_id = models.PositiveIntegerField()
+	content_object = GenericForeignKey('content_type', 'object_id')
+	deny = models.BooleanField(default=False)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	
 	def __unicode__(self):
--- tukiogram/models.py
+++ tukiogram/models.py
@@ -13,7 +13,7 @@ class Tukio(models.Model):
 	)
 	user = models.ForeignKey(settings.AUTH_USER_MODEL)
 	category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
-	desc = models.TextField()
+	desc = models.CharField(max_length=70)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	location_name = models.CharField(max_length=100, null=True)
 	location_geom = models.PointField(srid=4326)
